3f00fb340c217839ab842dbfec34bc78115e4789
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2026-03-28 17:26:36 -0300

    errors

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
  *  View Engine and Templates
@@ -27,7 +28,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index Route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
@@ -38,22 +39,30 @@ app.get("/error", (req, res, next) => {
 /* ***********************
  * 404 Handler
  *************************/
-app.use((req, res, next) => {
-  res.status(404).render("errors/error", {
-    title: "404 Not Found",
-    message: "Sorry, the page you are looking for does not exist."
-  })
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 
 /* ***********************
- * Error Handler
- *************************/
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).render("errors/error", {
-    title: "Server Error",
-    message: err.message,
-    nav: '' // agregamos nav vacío para que navigation.ejs no falle
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  
+  let message
+  if (err.status == 404) {
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
+
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
   })
 })
 
