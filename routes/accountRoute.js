71fc6f9221f5d4b1c0c17d39c3061c4af3800e3a
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2026-04-06 22:19:03 -0300

    learning activity 4

---
+++ routes/accountRoute.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+
+// Route to login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+// Route to registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+module.exports = router
